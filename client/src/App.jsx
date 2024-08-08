06f88bacc49a2802cd4181b741b531ac5a4c8d53
anouar garbaa <g.anouar@yahoo.com>
2024-08-08 20:40:35 +0100

    environment setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+  <div>
+  <Button>Click</Button>;
+  <button>click</button>
+  </div>
+  )
+};
+
+export default App;
