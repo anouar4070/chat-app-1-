6c4a5c2804d83fb8760f0c92e7dba3763ec2a1cf
anouar garbaa <g.anouar@yahoo.com>
2024-08-10 16:12:33 +0100

    add sign in & sign up UI

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,5 +1,11 @@
+import Background from "@/assets/login2.png";
+
 import React from "react";
-import Victory from "../../assets/victory.svg"
+import Victory from "../../assets/victory.svg";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 const Auth = () => {
   return (
     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
@@ -10,7 +16,62 @@ const Auth = () => {
               <h1 className="text-5xl md:text-6xl font-bold">Welcome</h1>
               <img src={Victory} className="h-[100px]" />
             </div>
+            <p className="font-medium text-center">
+              Fill in the details to get started with the best chat-app
+            </p>
           </div>
+          <div className="flex items-center justify-center w-full">
+            <Tabs className="w-3/4">
+              <TabsList className="bg-transparent rounded-none w-full">
+                <TabsTrigger
+                  value="login"
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300  "
+                >
+                  Login
+                </TabsTrigger>
+                <TabsTrigger
+                  value="signup"
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300  "
+                >
+                  Sign up
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="login" className="flex flex-col gap-5 mt-10">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6 "
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6 "
+                />
+                <Button className="rounded-full p-6 "> Login</Button>
+              </TabsContent>
+              <TabsContent value="signup" className="flex flex-col gap-5 ">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6 "
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6 "
+                />
+                <Input
+                  placeholder="Confirm Password"
+                  type="password"
+                  className="rounded-full p-6 "
+                />
+                <Button className="rounded-full p-6 ">Sign up</Button>
+              </TabsContent>
+            </Tabs>
+          </div>
+        </div>
+        <div className="hidden xl:flex justify-center items-center ">
+          <img src={Background} className="h-[500px]" />
         </div>
       </div>
     </div>
